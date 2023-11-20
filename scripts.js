9396383d358b3ccdc32a1a8acbe87b370210bd9c
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2023-11-20 18:02:20 +0100

    Cleaner code
    Since I'm no longer wondering whether the problem was my code or the server (it was the server)

--- scripts.js
+++ scripts.js
@@ -1,23 +1,7 @@
 /* accdss the photo-layout ul, then for each item in the json file, create a li and innerhtml the info in the JSON into it */
 
-let layoutUl = document.getElementsByClassName("photo-layout");
+fetch("http://localhost:3000/photo-layout-images")
+  .then(res=>res.json())
+  .then(json=> console.log(json))
 
-let promise = fetch('http://localhost:3000/photo-layout-images', {
-        headers: {
-          'Access-Control-Allow-Origin': 'http://127.0.0.1:5500/index.html',
-          'Content-Type': 'application/json'
-        },
-        method: 'GET',
-})
-
-promise
-  .then(response => {
-    console.log(response)
-
-    let someOtherPromise = response.text()
-
-    return someOtherPromise
-  })
-  .then(result => {
-    console.log(result)
-  })
+let layoutUl = document.querySelector('photo-layout')
