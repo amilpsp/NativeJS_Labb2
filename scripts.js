27419884fdf76eab9f63a9b5e15472f4c807af45
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2023-11-20 18:32:08 +0100

    effectively created a new node in ul, will make a function from this

--- scripts.js
+++ scripts.js
@@ -2,6 +2,12 @@
 
 fetch("http://localhost:3000/photo-layout-images")
   .then(res=>res.json())
-  .then(json=> console.log(json))
+  .then(json=> console.log(json));
 
-let layoutUl = document.querySelector('photo-layout')
+  /* creating a new li, putting the image in it, and appending it to the list */
+let layoutUl = document.getElementById('photo-layout')
+let newLi = document.createElement('li')
+let newTextNode = document.createTextNode('')
+newLi.innerHTML=`<img src="img/compressed_buildings-1836478_1920.jpg" alt="brick facade and the entrance of a caffee">`
+newLi.appendChild(newTextNode)
+layoutUl.appendChild(newLi)
