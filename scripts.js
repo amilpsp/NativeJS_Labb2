835508dfa2bb7280b113ee738065dac830068e65
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2023-11-22 19:01:35 +0100

    Gave up on pic-preview modals. Must be clashing with Bootstrap and I don't have time for the documentation.

--- scripts.js
+++ scripts.js
@@ -19,48 +19,4 @@ function newPicture (src, alt, srcHD){
 
     newLi.appendChild(newTextNode)
     layoutUl.appendChild(newLi)
-    newModal(srcHD, alt) 
-    
-    /* BUGGED making a new modal for each picture created 
-    
-    calling it directly from here because their creation depends on this
-
-    [ominously religious phrasing aside]
-    
-    */
 }
-
-function newModal (srcHD,alt){
-    let layoutModals = document.getElementById('modals-container')
-    let newDiv = document.createElement('div')
-    let newTextNode = document.createTextNode('')
-    newDiv.innerHTML=`
-<div   
-class="photo-modal modal fade"
-id="exampleModal1">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="ratio ratio-16x9">
-                <img
-                src="${srcHD}"
-                alt="${alt}"
-                allowfullscreen
-                ></img>
-            </div> <!-- end of ratio -->
-        </div> <!-- end of modal-content -->
-    </div> <!-- end of modal-dialog -->
-</div> <!-- end of modal -->`
-    newDiv.appendChild(newTextNode)
-    layoutModals.appendChild(newDiv)
-}
-
-/* event listener for the modal display */
-addEventListener('DOMContentLoaded', () => {
-    function displayModal(e){
-        e.style.display="float"
-    }
-    
-    let layoutImages = document.querySelectorAll('.layout-img')
-    layoutImages.addEventListener('click',displayModal(this))
-    }
-)
